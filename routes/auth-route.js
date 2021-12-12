18b279deb540ead5ad63b42b76e25b632d7a0ed8
steven <steven_ko@paradise-soft.com.tw>
2021-12-12 16:30:04 +0800

    feat: 加上註冊頁面

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,8 +1,34 @@
 const router = require('express').Router()
 const passport = require('passport')
+const bcrypt = require('bcrypt')
+const User = require('../models/user-model')
 
 router.get('/login', (req, res) => {
   res.render('login', { user: req.user })
+})
+
+router.get('/signup', (req, res) => {
+  res.render('signup', { user: req.user })
+})
+
+router.post('/signup', async (req, res) => {
+  let { name, email, password } = req.body
+  // check if the data is already in db
+  const emailExist = await User.findOne({ email })
+  if (emailExist) return res.status(400).send('Eamil already exist.')
+
+  const hash = await bcrypt.hash(password, 10)
+  password = hash
+  let newUser = new User({ name, email, password })
+  try {
+    const savedUser = await newUser.save()
+    res.status(200).send({
+      msg: 'User saved.',
+      savedObj: savedUser
+    })
+  } catch(err) {
+    res.status(400).send(err) 
+  }
 })
 
 router.get('/logout', (req, res) => {
