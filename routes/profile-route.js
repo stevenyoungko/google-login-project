e8923d907fcd7fe1e13c461f0a9c26353528180d
steven <steven_ko@paradise-soft.com.tw>
2021-12-12 17:51:22 +0800

    feat: add post model

--- routes/profile-route.js
+++ routes/profile-route.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const Post = require('../models/post-model')
 
 const authCheck = (req, res, next) => {
   if (!req.isAuthenticated()) {
@@ -8,8 +9,25 @@ const authCheck = (req, res, next) => {
   }
 }
 
-router.get('/', authCheck, (req, res) => {
-  res.render('profile', { user: req.user })
+router.get('/', authCheck, async (req, res) => {
+  let postFound = await Post.find({ author: req.user._id })
+  res.render('profile', { user: req.user, posts: postFound })
 })
+
+router.get('/post', authCheck, (req, res) => {
+  res.render('post', { user: req.user })
+})
+
+router.post('/post', authCheck, async (req, res) => {
+  let { title, content } = req.body
+  let newPost = new Post({ title, content, author: req.user._id })
+  try {
+    await newPost.save()
+    res.status(200).redirect("/profile")
+  } catch(err) {
+    req.flash("error_msg", "Both title and content are required.")
+    res.redirect("/profile/post")
+  }
+ })
 
 module.exports = router
